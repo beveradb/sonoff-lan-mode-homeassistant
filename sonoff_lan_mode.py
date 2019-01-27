4f4ccec23047bffd5938e2991edb1cff84de4204
Andrew Beveridge <andrew@beveridge.uk>
2019-01-27 19:54:07 +0000

    Renamed variables, functions and reformatted to follow PEP8, fixed import order to support HASS lazy-loading, and fixed return value and function call to correct errors.

--- sonoff_lan_mode.py
+++ sonoff_lan_mode.py
@@ -10,7 +10,6 @@ from homeassistant.const import (CONF_NAME, CONF_HOST, CONF_ID, CONF_SWITCHES, C
 import homeassistant.helpers.config_validation as cv
 from time import time
 import json
-from websocket import create_connection
 from venv import logger
 
 REQUIREMENTS = ['websocket-client==0.54.0']
@@ -30,7 +29,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 
-def set_sonoff_state(sonoffDevice, state):
+def set_sonoff_state(sonoff_device, state):
     timestamp = str(time()).replace('.', '')
     update_state_message_json = json.dumps(
         {
@@ -47,15 +46,15 @@ def set_sonoff_state(sonoffDevice, state):
     )
 
     print("Updating state: " + update_state_message_json)
-    sonoffDevice.GetWS().send(update_state_message_json)
+    sonoff_device.get_ws.send(update_state_message_json)
 
-    response_message = sonoffDevice.GetWS().recv()
+    response_message = sonoff_device.get_ws.recv()
     print("Response: '%s'" % response_message)
 
 
 class SonoffDevice(SwitchDevice):
-
     """Representation of a Sonoff switch."""
+
     def __init__(self, host, port, name, icon):
         """Initialize the Sonoff switch."""
         self._host = host
@@ -82,26 +81,26 @@ class SonoffDevice(SwitchDevice):
         return self._icon
 
     @property
-    def GetWS(self):
+    def get_ws(self):
         return self._ws
 
     def turn_on(self, **kwargs):
         """Turn Sonoff switch on."""
-        set_sonoff_state(self._host, "on")
+        set_sonoff_state(self, "on")
         self._state = True
 
     def turn_off(self, **kwargs):
         """Turn Sonoff switch off."""
-        set_sonoff_state(self._host, "off")
+        set_sonoff_state(self, "off")
         self._state = False
 
     def init_ws_connection(self):
-
+        from websocket import create_connection
         timestamp = str(time()).replace('.', '')
 
-        wsAddress = "ws://" + self._host + ":8081/"
+        ws_address = "ws://" + self._host + ":8081/"
         self._ws = create_connection("ws://" + self._host + ":8081/")
-        logger.info("connecting to " + wsAddress)
+        logger.info("connecting to " + ws_address)
         initiate_session_message_json = json.dumps(
             {
                 "action": "userOnline",
@@ -125,19 +124,20 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     switches = []
 
     for object_id, device_config in devices.items():
-        add_sonoff_devie(config, 8081, device_config.get(CONF_FRIENDLY_NAME, object_id))
+        add_sonoff_device(config, 8081, device_config.get(CONF_FRIENDLY_NAME, object_id))
 
     name = config.get(CONF_NAME)
 
     if name:
-        switches.append(add_sonoff_devie(config, 8081, name))
+        switches.append(add_sonoff_device(config, 8081, name))
 
     add_devices(switches)
 
-def add_sonoff_devie(config, port, name):
-    SonoffDevice(
+
+def add_sonoff_device(config, port, name):
+    return SonoffDevice(
         config.get(CONF_HOST),
-        8081,
+        port,
         name,
         config.get(CONF_ICON)
     )
