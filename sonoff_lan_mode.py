2d7c6f62ec28b5bc7efe5ba4b43b55f68ab799c3
Andrew Beveridge <andrew@beveridge.uk>
2019-02-02 02:33:22 +0000

    Added websockets minimum version requirement

--- sonoff_lan_mode.py
+++ sonoff_lan_mode.py
@@ -13,7 +13,7 @@ import voluptuous as vol
 from homeassistant.components.switch import (SwitchDevice, PLATFORM_SCHEMA)
 from homeassistant.const import CONF_HOST, CONF_NAME
 
-REQUIREMENTS = ['pysonofflan>=0.2.0']
+REQUIREMENTS = ['pysonofflan>=0.2.0', 'websockets>=7.0']
 
 _LOGGER = logging.getLogger('homeassistant.components.switch.sonoff_lan_mode')
 
