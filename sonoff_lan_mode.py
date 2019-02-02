897f0106748cc5856777e4b239e4307d52039a04
Andrew Beveridge <andrew@beveridge.uk>
2019-02-02 00:33:31 +0000

    First working async version, updating device state through pysonofflan library

--- sonoff_lan_mode.py
+++ sonoff_lan_mode.py
@@ -11,11 +11,11 @@ import logging
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 from homeassistant.components.switch import (SwitchDevice, PLATFORM_SCHEMA)
-from homeassistant.const import (CONF_HOST, CONF_NAME)
+from homeassistant.const import CONF_HOST, CONF_NAME
 
 REQUIREMENTS = ['pysonofflan>=0.1.7']
 
-_LOGGER = logging.getLogger(__name__)
+_LOGGER = logging.getLogger('homeassistant.components.switch.sonoff_lan_mode')
 
 DEFAULT_NAME = 'Sonoff Switch'
 
@@ -25,65 +25,79 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+async def async_setup_platform(hass, config, async_add_entities,
+                               discovery_info=None):
     """Set up the Sonoff LAN Mode Switch platform."""
     host = config.get(CONF_HOST)
     name = config.get(CONF_NAME)
 
-    add_entities([HassSonoffSwitch(host, name)], True)
+    async_add_entities([HassSonoffSwitch(hass, host, name)], True)
 
 
 class HassSonoffSwitch(SwitchDevice):
     """Home Assistant representation of a Sonoff LAN Mode device."""
 
-    def __init__(self, host, name):
+    def __init__(self, hass, host, name):
         from pysonofflan import SonoffSwitch
+
+        _LOGGER.setLevel(logging.DEBUG)
+
         self._name = name
         self._state = None
         self._available = True
-        self.shared_state = {"hass_self": self}
-        self.sonoffdevice = SonoffSwitch(
+        self._shared_state = {}
+        self._sonoff_device = SonoffSwitch(
             host=host,
             callback_after_update=self.device_update_callback,
-            shared_state=self.shared_state,
-            logger=_LOGGER
+            shared_state=self._shared_state,
+            logger=_LOGGER,
+            loop=hass.loop,
+            ping_interval=145
         )
+
+        _LOGGER.info("HassSonoffSwitch __init__ finished creating "
+                     "SonoffSwitch")
 
     @property
     def name(self):
+        _LOGGER.info("HassSonoffSwitch returning _name: %s" % self._name)
         return self._name
 
     @property
     def available(self) -> bool:
         """Return if switch is available."""
+        _LOGGER.info("HassSonoffSwitch returning _available: %s" %
+                     self._available)
         return self._available
 
     @property
     def is_on(self):
         """Return true if switch is on."""
+        _LOGGER.info("HassSonoffSwitch returning _state: %s" % self._state)
         return self._state
 
     async def turn_on(self, **kwargs):
         """Turn the switch on."""
-        await self.sonoffdevice.turn_on()
+        _LOGGER.info("HassSonoffSwitch turn_on called")
+        await self._sonoff_device.turn_on()
 
     async def turn_off(self, **kwargs):
         """Turn the switch off."""
-        await self.sonoffdevice.turn_off()
+        _LOGGER.info("HassSonoffSwitch turn_off called")
+        await self._sonoff_device.turn_off()
 
     async def device_update_callback(self, callback_self):
         """Handle state updates announced by the device itself."""
-        self._state = \
-            self.sonoffdevice.state == \
-            self.sonoffdevice.SWITCH_STATE_ON
+        _LOGGER.info("HassSonoffSwitch device_update_callback called")
+        await self.async_update()
 
-    async def update(self):
+    async def async_update(self):
         """Update the device state."""
+        _LOGGER.info("HassSonoffSwitch async_update called")
         try:
-            self._state = self.sonoffdevice.state == \
-                          self.sonoffdevice.SWITCH_STATE_ON
-        except (Exception, OSError) as ex:
+            self._state = self._sonoff_device.state == \
+                          self._sonoff_device.SWITCH_STATE_ON
+        except Exception as ex:
             if self._available:
                 _LOGGER.warning(
                     "Could not read state for %s: %s", self.name, ex)
-                self._available = False
