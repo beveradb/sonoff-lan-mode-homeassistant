c2724b2c6200b03ab3e36f4638dac5c5e535a109
mattsaxon <saxonmatt@hotmail.com>
2019-09-11 18:32:07 +0100

    Added __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+sonoff_lan_mode_r3
+"""
+
+__author__ = """Andrew Beveridge"""
+__email__ = 'andrew@beveridge.uk'
+__version__ = '0.4.0'
+__url__ = 'https://github.com/mattsaxon/sonoff_lan_mode'
+
+
