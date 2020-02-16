3c75b4039f710bba9686ce5f7bc99a289eef2003
mattsaxon <saxonmatt@hotmail.com>
2020-02-16 16:45:44 +0000

    1.1.3

--- sonoff_lan_mode_r3/__init__.py
+++ sonoff_lan_mode_r3/__init__.py
@@ -6,7 +6,7 @@ sonoff_lan_mode_r3
 
 __author__ = """Matt Saxon"""
 __email__ = 'saxonmatt@hotmail.com'
-__version__ = '1.1.1'
+__version__ = '1.1.3'
 __url__ = 'https://github.com/mattsaxon/sonoff-lan-mode-homeassistant'
 
 
